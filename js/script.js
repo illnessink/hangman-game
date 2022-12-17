1c75d0669a473747a3021b1c78e566509ad87aac
Vanessa Foerster <nessfoerster@gmail.com>
2022-12-17 17:42:44 -0500

    wip: adds changing img

--- js/script.js
+++ js/script.js
@@ -8,6 +8,8 @@ const wordDisplay = [];
 const button = $('.alphButton');
 let randomWord = '';
 let winOrLose = $('#winLose');
+let hangman = $('#hangmanImg');
+// console.log(hangman.attr("src"));
 
 
 $.ajax({
@@ -38,7 +40,9 @@ function showLetter(word, letter) {
                     winCount += 1;
                     console.log(`win count: ${winCount}`)
                     if (winCount === word.length) {
-                            winOrLose.text(`HURRAH, YOU WON! THE MAN WAS SAVED! `)
+                            winOrLose.text(`HURRAH, YOU WON! THE MAN WAS SAVED! `);
+                            hangman.attr("src","https://i.imgur.com/DjpNAHA.jpg");
+                            console.log(hangman.src)
                         }
                 }  
             })
@@ -49,14 +53,6 @@ function letterGuess(word, letter) {
     if (word.includes(letter)) {
         console.log(`The word ${word} contains the letter ${letter}`)
         showLetter(word, letter)
-        // winCount += 1;
-        // console.log(`win count: ${winCount}`)
-        // if (winCount === word.length) {
-        //     // let letterArray = word.split("");
-        //     winOrLose.text(`HURRAH, YOU WON! THE WORD WAS ${word.toUpperCase()}! `)
-        // }
-        
-        // console.log(word, letter)
     } else {
         guesses -= 1
         console.log(`guesses: ${guesses}`)
@@ -66,9 +62,19 @@ function letterGuess(word, letter) {
 }
 
 function winOrLoseCheck(guess, word) {
-    // let letterArray = word.split("");
-    if (guess === 0) {
-        winOrLose.text(`SORRY, YOU LOST! THE WORD WAS ${word.toUpperCase()}! `)
+    if (guess === 5) {
+      hangman.attr("src", "https://i.imgur.com/xnDztPs.jpg");
+    } else if (guess === 4) {
+      hangman.attr("src", "https://i.imgur.com/lE6uNCK.jpg");
+    } else if (guess === 3) {
+      hangman.attr("src", "https://i.imgur.com/lySlp3q.jpg");
+    } else if (guess === 2) {
+      hangman.attr("src", "https://i.imgur.com/i4yhyIc.jpg");
+    } else if (guess === 1) {
+      hangman.attr("src", "https://i.imgur.com/HTVr7cd.jpg");
+    } else if (guess === 0) {
+        winOrLose.text(`SORRY, YOU LOST! THE WORD WAS ${word.toUpperCase()}! `);
+        hangman.attr("src", "https://i.imgur.com/Ybj5Tgn.jpg");
     } 
 }
 
@@ -90,10 +96,3 @@ button.on("click", function(evt) {
     // console.log(wordDisplay);
     return wordDisplay.join(" ");
   }
-
-
-  
-
-//   console.log(wordDisplay);
-//   console.log(answer);
-//   console.log(str)
